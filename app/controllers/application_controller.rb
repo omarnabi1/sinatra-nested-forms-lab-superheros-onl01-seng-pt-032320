f8b4f16c09c183e158342b1e41fb43b6bd6fd851
Omar Nabi <onabi20@gmail.com>
2020-06-22 19:55:37 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,25 @@ class App < Sinatra::Base
       erb :super_hero
     end
     
+    
+    post '/teams' do
 
+
+    @team_name = params[:team][:name]
+    @team_motto = params[:team][:motto]
+    @hero_name = []
+    @hero_power = []
+    @hero_bio = []
+    @team_members = params[:team][:members]
+    @team_members.each do |hero|  #<----------problem with the each iterator
+      @hero_name << hero[:name]
+      @hero_power << hero[:power]
+      @hero_bio << hero[:bio]
+    end
+
+
+    erb :team
+  end
 end
+
+
